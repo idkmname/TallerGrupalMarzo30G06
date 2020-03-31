7af4dc121fe4c865b00475f58555f959dec48f27
juanjo145 <54655155+juanjo145@users.noreply.github.com>
2020-03-31 17:10:38 -0500

    Cambio de tienda.h

---
+++ tienda.h
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+enum tipoProducto{
+	LEGUMBRES, ENLATADOS, CEREALES, DULCES
+};
+
+typedef struct producto{
+	char nombre[20];
+	int precio;
+	int tipo;	
+}producto;
+
+
+//Inicializa la matriz con valores estandar 
+void inicializar(producto matriz[][5], int fil, int col);
+
+//Agregar un producto a la matriz de un tipo definido
+void agregarProducto( producto matriz[][5] );
