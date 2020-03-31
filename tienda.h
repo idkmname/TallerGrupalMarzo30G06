e63fda1e760ffe2f9ffa142bcc4cd85378ce8fb2
idkmname <57722166+idkmname@github.com>
2020-03-31 16:19:24 -0500

    Definicion de enums y primera funcion

--- tienda.h
+++ tienda.h
@@ -1,11 +1,16 @@
 #include<stdio.h>
 #include<string.h>
 
+typedef enum tipoProducto{
+	LEGUMBRES, ENLATADOS, CEREALES, DULCES
+};
+
 typedef struct producto{
 	char nombre[20];
 	int precio;
-	int tipo;
-	
+	int tipo;	
 }producto;
 
 
+//Agregar un producto a la matriz de un tipo definido
+void agregarProducto( producto matriz[][] );
