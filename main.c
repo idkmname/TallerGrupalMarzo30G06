08cc68d9a4713796b383312be13a6c544129988a
Danny Vargas Martinez <57722166+idkmname@users.noreply.github.com>
2020-03-31 17:53:15 -0500

    fix

---
+++ main.c
@@ -0,0 +1,22 @@
+#include "tienda.c"
+
+int main(){
+   int opc;
+   
+   producto matriz[4][4];
+   producto 
+   do{
+      opc = menu();
+      switch(){   
+	case 1: agregarProducto(matriz);
+		break;
+
+        case 2: break;
+
+	case 3: break;
+
+      }
+   }while( opc != 0 );
+   return
+
+}
